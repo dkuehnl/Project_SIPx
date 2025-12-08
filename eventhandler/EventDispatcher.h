65d00dbc0b7d9f90337622bceeed3e1c8b2bb1e1
dkuehnlein <d.kuehnlein@outlook.com>
2025-12-08 12:39:48 +0100

    added documentation

--- eventhandler/EventDispatcher.h
+++ eventhandler/EventDispatcher.h
@@ -1,6 +1,47 @@
-//
-// Created by dkueh on 27.11.2025.
-//
+/*
+*  Project SIPx
+ *  -------------
+ *  SIPx is a flexible and extensible SIP engine designed primarily for
+ *  troubleshooting, edge-case testing, and IMS-related scenarios. It provides
+ *  developers and DevOps engineers fine-grained control over SIP behavior,
+ *  including non-RFC-compliant flows and corner-case simulations.
+ *
+ *  Description of EventDispatcher:
+ *  -------------------------------
+ *  The EventDispatcher is a lightweight component used for broadcasting events
+ *  between SIPx modules. It implements a minimal publish/subscribe mechanism,
+ *  allowing any number of EventHandler instances to register themselves and
+ *  receive Event objects.
+ *
+ *  Main responsibilities:
+ *    • Maintain a list of registered EventHandler listeners
+ *    • Dispatch Event objects to all listeners in registration order
+ *
+ *  Internal structure:
+ *    • Stores raw pointers to EventHandler instances (ownership is not managed)
+ *    • Callers are responsible for ensuring the lifetime of handlers
+ *
+ *  Purpose and usage:
+ *    This dispatcher enables loose coupling between modules such as the parser,
+ *    logwriter, and future SIP logic components. It allows modules to react to
+ *    internal events without introducing direct dependencies.
+ *
+ *  Author:  Dennis Kühnlein
+ *  Version: v0.1
+ *  Created: 08.12.2025
+ *
+ *  License:
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
 
 #ifndef PROJECT_SIPX_EVENTDISPATCHER_H
 #define PROJECT_SIPX_EVENTDISPATCHER_H
--- eventhandler/EventHandler.h
+++ eventhandler/EventHandler.h
@@ -1,6 +1,46 @@
-//
-// Created by dkueh on 27.11.2025.
-//
+/*
+*  Project SIPx
+ *  -------------
+ *  SIPx is a flexible and extensible SIP engine designed primarily for
+ *  troubleshooting, edge-case testing, and IMS-related scenarios. It provides
+ *  developers and DevOps engineers fine-grained control over SIP behavior,
+ *  including non-RFC-compliant flows and corner-case simulations.
+ *
+ *  Description of EventHandler:
+ *  ----------------------------
+ *  EventHandler is the base interface for all components that want to receive
+ *  events dispatched through the SIPx event system. Any module that needs to
+ *  react to internal framework events must implement this class.
+ *
+ *  Main responsibilities:
+ *    • Define the on_event(...) callback for handling dispatched Event objects
+ *    • Provide a common interface for all event-reactive SIPx components
+ *
+ *  Internal structure:
+ *    • Abstract class containing only a virtual destructor and the event callback
+ *    • No state is stored; all behavior is implemented in derived classes
+ *
+ *  Purpose and usage:
+ *    Modules register their EventHandler-derived objects at an EventDispatcher
+ *    instance. Whenever the dispatcher broadcasts an Event, the on_event(...)
+ *    method of each registered handler is invoked.
+ *
+ *  Author:  Dennis Kühnlein
+ *  Version: v0.1
+ *  Created: 08.12.2025
+ *
+ *  License:
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
 
 #ifndef PROJECT_SIPX_EVENTHANDLER_H
 #define PROJECT_SIPX_EVENTHANDLER_H
